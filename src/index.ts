713695e8e999ad66eef3559e9e9794776dc7b323
Ludovic Muller <ludovicmuller1@gmail.com>
2021-07-07 12:20:54 +0200

    check existance of the script

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 import fastify from 'fastify';
+import fs from 'fs/promises';
 
 const server = fastify();
 const port = 3000;
 const host = '::';
+const scriptsPath = './scripts/';
 
-server.get('/run/:script_name', async (request, reply) => 'ok');
+server.get<{ Params: { script_name: string } }>('/run/:script_name', async (request, reply) => {
+  const name = request.params.script_name.replace(/^(\.)+/, '');
+  let fileInfos;
+  try {
+    fileInfos = await fs.stat(`${scriptsPath}/${name}.sh`);
+  } catch (_e) {
+    return reply.code(404).send({
+      success: false,
+      message: 'script not found',
+    });
+  }
+
+  if (!fileInfos.isFile()) {
+    return reply.code(400).send({
+      success: false,
+      message: 'not a file',
+    });
+  }
+
+  return fileInfos;
+});
 
 server.listen(port, host, (err, address) => {
   if (err) {
