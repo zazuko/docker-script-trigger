fb1118a31481b2bb3dd33be65baa2a39906acd95
Quentin Gliech <quentingliech@gmail.com>
2021-09-15 11:27:09 +0200

    feat: install the scripts at startup time
    This avoids weird arbitrary code execution and helps debugging

--- src/index.js
+++ src/index.js
@@ -2,79 +2,79 @@
 
 const fastify = require('fastify');
 const fs = require('fs/promises');
+const { promisify } = require('util');
+const glob = promisify(require('glob'));
 const { exec } = require('child_process');
+const path = require('path');
 
-const server = fastify();
 const port = process.env.SERVER_PORT || 3000;
 const host = process.env.SERVER_HOST || '::';
 const scriptsPath = process.env.SCRIPTS_PATH || './scripts/';
 
-server.post(
-  '/run/:script_name',
-  async (request, reply) => {
-    const name = request.params.script_name.replace(/^(\.)+/, '');
-    const scriptPath = `${scriptsPath}/${name}.sh`;
-    const args = request.body;
+const route = script => async (request, reply) => {
+  const args = request.body || [];
 
-    let fileInfos;
+  if (!Array.isArray(args)) {
+    return reply.code(400).send({
+      success: false,
+      message: 'expected args in an array'
+    });
+  }
 
-    try {
-      fileInfos = await fs.stat(scriptPath);
-    } catch (_e) {
-      return reply.code(404).send({
-        success: false,
-        message: 'script not found',
-      });
-    }
+  const argsString = args
+    .map(arg => `"${arg}"`) // TODO: escape
+    .join(' ')
+
+  try {
+    const { stdout, stderr } = await new Promise(
+      // TODO: use spawn instead of exec
+      (resolve, reject) => exec(`"${script}" ${argsString}`, (error, out, err) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({ stdout: out, stderr: err });
+        }
+      }),
+    );
+
+    return reply.code(200).send({
+      success: true,
+      stdout,
+      stderr,
+    });
+  } catch (e) {
+    return reply.code(500).send({
+      success: false,
+      message: e.message,
+    });
+  }
+};
+
+(async () => {
+  const server = fastify();
+  const files = await glob(`${scriptsPath}/*`);
+
+  for (const name of files) {
+    const realpath = await fs.realpath(name);
+    const fileInfos = await fs.stat(realpath);
 
     if (!fileInfos.isFile()) {
-      return reply.code(400).send({
-        success: false,
-        message: 'not a file',
-      });
+      throw new Error(`path ${realpath} is not a regular file`);
     }
 
-    if (!Array.isArray(args)) {
-      return reply.code(400).send({
-        success: false,
-        message: 'expected args in an array'
-      });
-    }
-
-    const argsString = args
-      .map(arg => `"${arg}"`) // TODO: escape
-      .join(' ')
-
-    try {
-      const { stdout, stderr } = await new Promise(
-        // TODO: use spawn instead of exec
-        (resolve, reject) => exec(`"${scriptPath}" ${argsString}`, (error, out, err) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve({ stdout: out, stderr: err });
-          }
-        }),
-      );
-
-      return reply.code(200).send({
-        success: true,
-        stdout,
-        stderr,
-      });
-    } catch (e) {
-      return reply.code(500).send({
-        success: false,
-        message: e.message,
-      });
-    }
-  },
-);
-
-server.listen(port, host, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
+    const parsedpath = path.parse(name);
+    console.log(`Route /run/${parsedpath.name} runs ${realpath}`);
+    server.post(`/run/${parsedpath.name}`, route(realpath));
   }
+
+  const address = await new Promise((resolve, reject) => server.listen(port, host, (err, address) => {
+    if (err) reject(err);
+    else resolve(address);
+  }));
+
   console.log(`Server listening at ${address}`);
+})().catch(e => {
+  console.error(e);
+  process.exit(1);
 });
+
